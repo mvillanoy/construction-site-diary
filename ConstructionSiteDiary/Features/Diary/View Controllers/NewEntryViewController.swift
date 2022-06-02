dc09ee5ec1f9ae5c88c25620c79f0bd6b744e545
Monica Villanoy <monicavillanoy@gmail.com>
2022-06-02 23:18:19 +0800

    Initial Project

---
+++ ConstructionSiteDiary/Features/Diary/View Controllers/NewEntryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewEntryViewController.swift
+//  ConstructionSiteDiary
+//
+//  Created by Monica Villanoy on 6/2/22.
+//
+
+import UIKit
+
+
+class NewEntryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ConstructionSiteDiary/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ConstructionSiteDiary
+//
+//  Created by Monica Villanoy on 6/2/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addEntryPressed(_ sender: Any) {
+        
+    }
+    
+}
+
